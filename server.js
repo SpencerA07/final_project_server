1f9a79cde9a6699bf5f9f47c70f2ed3d40a952e3
Rebecca <rebeccaw@MSI.localdomain>
2019-07-15 12:23:03 -0600

    New server stuff

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+    
 const express = require("express");
 const cors = require("cors");
 // const uuid = require("uuid");
@@ -19,6 +20,28 @@ server.use(function(req, res, next) {
 var data = require("./data.js");
 
 // Endpoints
+server.get("/test", function(req, res) {
+  var response = {
+    test: data.test
+  };
+  res.json(response);
+});
+
+// GET endpoint to retrieve user data
+server.get("/money", function(req, res) {
+  var response = {
+    userdata: data.users
+  };
+  response.userdata.forEach(function(user) {
+    user.totalMonies = user.savingsAcct + user.checkingAcct;
+  });
+  res.json(response);
+});
+
+// POST endpoind to add new user data
+server.post("/money", function(req, res) {
+  
+});
 
 // Start the server
 server.listen(port, function() {
